553ed711913c46fad5c2c87f5230c9b7a39f457b
Paco Mastermind <paco@mastermind.ac>
2023-09-06 11:38:29 +0200

    Primer componente

---
+++ app/components/banner.js
@@ -0,0 +1,11 @@
+
+const Banner = () =>{
+    return(
+        <header>
+            <div><img src="./logo.jpg" alt="Logo Esqueleto"></img></div>
+            <div>Enseñando tecnlologia</div>
+        </header>
+    )
+
+  };
+  export default Banner;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Image from 'next/image'
+import Banner from './components/Banner'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Comenzando con React</h1>
-    </main>
+    <>
+      <Banner/>
+      <main className={styles.main}>
+        <h1>Comenzando con React</h1>
+      </main>
+    </>
   )
 }
